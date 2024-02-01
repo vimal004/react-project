aa3f9108b556068505ce82f66f7a55adaaee95f0
Vimal Manoharan <2004.vimal@gmail.com>
2024-02-02 00:39:10 +0530

    My First React Repo

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+const root=ReactDOM.createRoot(document.getElementById("root"));
+const parent=React.createElement("h1",{},"Hello guys");
+root.render(parent);
