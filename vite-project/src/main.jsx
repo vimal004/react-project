c3380315ea40db420347f268ba592396ead679de
Vimal Manoharan <2004.vimal@gmail.com>
2024-02-09 15:03:36 +0530

    committed

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-const root=ReactDOM.createRoot(document.getElementById("root"));
-const parent=React.createElement("h1",{},"Hello guys");
-root.render(parent);
+const Header=()=>{
+    return (
+        <div className='hr'>
+            <img src='https://www.vhv.rs/dpng/d/433-4338596_swiggy-logo-png-image-free-download-searchpng-swiggy.png' id='img'></img>
+            <nav>About</nav>
+            <nav>Cuisines</nav>
+            <nav>Restaurants</nav>
+            <nav>Contact</nav>
+        </div>
+    )
+}
+const AppLayout=()=>{
+    return (
+        <Header></Header>
+    )
+}
+const root=ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<AppLayout></AppLayout>);
